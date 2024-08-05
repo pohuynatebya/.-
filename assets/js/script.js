250d55db5b73061d4d564386e28c1010dde7a4be
pohuynatebya <pohuynatebta12@gmail.com>
2024-08-05 17:54:43 +0500

    1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 'use strict';
 
 
-
 // element toggle function
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
@@ -157,3 +156,22 @@ for (let i = 0; i < formInputs.length; i++) {
 
 //   });
 // }
+
+
+// custom-cursor
+
+// const cursorRounded = document.querySelector('.rounded');
+// const cursorPointed = document.querySelector('.pointed');
+
+
+// const moveCursor = (e)=> {
+//   const mouseY = e.clientY;
+//   const mouseX = e.clientX;
+   
+//   cursorRounded.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+  
+//   cursorPointed.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+ 
+// }
+
+// window.addEventListener('mousemove', moveCursor)
